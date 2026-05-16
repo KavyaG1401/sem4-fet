8a1fd78d64b2e975971f69ebe69c2b4fdc111a77
kavya <kavyag4224@gmail.com>
2026-05-16 08:07:22 +0530

    Refactor loop functions and add layer controls

--- assignments/script.js
+++ assignments/script.js
@@ -1,28 +1,66 @@
-function whileLoop() {
 
-    let i = 1;
-    let result = "";
+function resetLayers() {
 
-    while (i <= 5) {
-        result += i + " ";
-        i++;
-    }
+    document.getElementById("red").style.zIndex = "0";
+    document.getElementById("blue").style.zIndex = "0";
+    document.getElementById("green").style.zIndex = "0";
 
-    document.getElementById("whileOutput").innerHTML =
-        "While Loop Output: " + result;
 }
 
-function doWhileLoop() {
+function showRed() {
 
-    let j = 1;
-    let result = "";
+    resetLayers();
+
+    document.getElementById("red").style.zIndex = "3";
+
+}
+
+function showBlue() {
+
+    resetLayers();
+
+    document.getElementById("blue").style.zIndex = "3";
+
+}
+
+function showGreen() {
+
+    resetLayers();
+
+    document.getElementById("green").style.zIndex = "3";
+
+}
+
+function autoSwitch() {
+
+    let layers = ["red", "blue", "green"];
+
+    let i = 0;
+
+    setInterval(function () {
+
+        resetLayers();
+
+        document.getElementById(layers[i]).style.zIndex = "3";
+
+        i++;
+
+        while (i >= layers.length) {
+
+            i = 0;
+
+        }
+
+    }, 1000);
+
+    let j = 0;
 
     do {
-        result += j + " ";
-        j++;
-    }
-    while (j <= 5);
 
-    document.getElementById("doOutput").innerHTML =
-        "Do While Loop Output: " + result;
+        j++;
+
+    }
+
+    while (j < 3);
+
 }
